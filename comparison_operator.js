b1ac241d3b60a32ef6a1ce66c0c3f70b6b7148d3
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2024-01-21 14:13:28 +0530

    Add files via upload

---
+++ comparison_operator.js
@@ -0,0 +1,18 @@
+//Coding wizard
+//comparsion operators
+// ==   --> equals to
+// !=   --> not equals
+// ===  --> equal value and type
+// !==  --> not equal value and type
+// >    --> greater than
+// <    --> less than
+// >=   --> greater than or equal to
+// <=   --> less than or equal to
+// ?    --> Ternary operator
+
+let a = 7
+let b = "7"
+console.log("A == B",a==b)
+console.log("A != B",a!=b)
+console.log("A === B",a===b)
+console.log("A !== B",a!==b)

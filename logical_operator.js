304fd9b452fb633f0081134847497ada4f8077c3
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2024-01-22 16:51:10 +0530

    Add files via upload

---
+++ logical_operator.js
@@ -0,0 +1,12 @@
+//coding wizard
+//Logical Operators
+// && --> Logical AND
+// || --> Logical OR
+// !  --> Logical NOT
+
+let a= 40
+let b =700
+// console.log(a==b && a==b)
+// console.log(a>b || a==b)
+console.log(!true)
+

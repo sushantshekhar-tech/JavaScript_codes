88327ad6d3ac3c80a69565a9c35c9fae0dba2d9a
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2024-01-25 16:06:50 +0530

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,16 @@
+// coding wizard
+// if-else condition statements
+
+let a = 20
+let b = 20
+if(a<b){
+    console.log("a is less than b")
+}
+else if(a==b){
+    console.log("a is equal to b")
+
+}
+else{
+    console.log("a is greater than b")
+
+}
---
+++ ternary_operator.js
@@ -0,0 +1,7 @@
+//coding wizard
+//Ternary operator
+
+// condition? Expx1 : exp2
+
+let age =12
+console.log(age>18?"You can drive":"You cannot Drive")

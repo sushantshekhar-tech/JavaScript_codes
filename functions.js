ecce49ee1c23aacc7c8efb2cdb5a7e90c1fac2b7
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2024-01-30 14:24:41 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,14 @@
+//coding wizard
+//Functions
+
+function greet(x){
+    console.log("Good morning",x)
+}
+greet("Neha")
+greet("sohan")
+greet("rohan")
+greet("rohan")
+greet("rohan")
+greet("rohan")
+greet("rohan")
+greet("rohan")
---
+++ print_a_to_z.js
@@ -0,0 +1,10 @@
+//coding wizard
+//JavaScript code to print "a" to "z"
+//of Engish Alphabet without using array
+
+console.log('a'.charCodeAt(0))
+console.log('z'.charCodeAt(0))
+
+for(let i=97;i<=122;i++){
+    console.log(String.fromCharCode(i))
+}

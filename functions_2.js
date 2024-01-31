79aa0dad67c18f2c6e1ffa0c236cd9b47536a8be
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2024-01-31 13:49:44 +0530

    Add files via upload

---
+++ functions_2.js
@@ -0,0 +1,12 @@
+//coding wizard
+// Functions part - 2
+
+// function multiply(x, y) {
+//   return x * y;
+// } explicit return
+
+const multiply =(x,y)=> x*y
+//implicit return
+
+
+console.log("The multiplication of two number is ", multiply(6, 10));
